18ed2c06623e7cd221a4e1dc075863191762e27b
friedflip <173607597+friedflip@users.noreply.github.com>
2025-09-03 14:40:59 +0800

    Wrote out pseudocode for JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+// FUNCTION
+// FOR getComputerChoice
+// ENDFOR Return strings "Rock", "Paper", or "Scissors"
+// ENDFUNCTION
+
+// INPUT
+// FUNCTION
+// FOR getHumanChoice 
+// ENDFOR Return getComputerChoice
+// ENDFUNCTION
+// OUTPUT
+
+// REPEAT playRound
+// UNTIL humanScore > computerScore OR computerScore > humanScore
