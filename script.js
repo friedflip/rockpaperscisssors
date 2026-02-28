abe7ddc431e860c255ded9e9c9b9e020da3502db
friedflip <173607597+friedflip@users.noreply.github.com>
2026-03-01 01:52:56 +0800

    Added page title

--- script.js
+++ script.js
@@ -12,3 +12,38 @@
 
 // REPEAT playRound
 // UNTIL humanScore > computerScore OR computerScore > humanScore
+
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomIndex = Math.floor(Math.random() *choices.length);
+    let computerChoice = choices(randomIndex);
+    return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+if (playerSelection === computerSelection) {
+    return "It's a tie!";
+} else if {
+(playerSelection === "Rock" && computerSelection === "Scissors") ||
+(playerSelection === "Paper" && computerSelection === "Rock") ||
+(playerSelection === "Scissors" && computerSelection === "Paper")
+} {
+    return "You win!" + playerSelection + " beats " + computerSelection;
+} else {
+    return "You lose!" + computerSelection + " beats " + playerSelection;
+}
+}
+
+ function game() {
+    let playerScore = 0;
+    let computerSCore = 0;
+
+for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Choose rock, paper, or scissors");
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+}
+ }
